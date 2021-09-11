a6a58ac710a13a5ec74b3ff73eea90021b19411a
polymurph <eddyed.k@gmail.com>
2021-09-11 14:10:06 +0200

    work on firmware

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <pthread.h>
 
+#include "reflowOven.hpp"
 
 int main(void)
 {
     std::cout << "Reflow Controll Firmware" << std::endl;
+
+    ReflowOven oven;
+
+    oven.getTemperatureCelsius();
+    oven.setFanDutycycle();
+    
+
+
+
 
     return 0;
 }
