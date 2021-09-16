7906fa7ec349cc6f673f0dc492bb15b86720e3b5
polymurph <eddyed.k@gmail.com>
2021-09-16 18:26:21 +0200

    working fsm structure

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -2,20 +2,49 @@
 #include <pthread.h>
 
 #include "reflowOven.hpp"
+#include "reflowOvenCtrl.hpp"
+
+char getCommand();
 
 int main(void)
 {
+    char cmd;
     std::cout << "Reflow Controll Firmware" << std::endl;
 
-    ReflowOven oven;
-
-    oven.getTemperatureCelsius();
-    oven.setFanDutycycle();
-    
-    oven.turnOffFan();
+    ReflowOvenCtrl reflowCtrl;
 
 
+    do{
+        cmd = getCommand();
+        switch(cmd)
+        {
+            case 'a':
+                reflowCtrl.process(ReflowOvenCtrl::Event::evAbort);
+                break;
+            case 's':
+                reflowCtrl.process(ReflowOvenCtrl::Event::evStartReflowProcess);
+                break;
+            default:
+                break;
+        }
 
+    }while(cmd != 'q');
 
     return 0;
+}
+
+char getCommand()
+{
+    char answer;
+    std::cout << std::endl << "-------------------------------------------" << std::endl;
+    std::cout << "    a   Abort" << std::endl;
+    std::cout << "    s   Start Reflow Process" << std::endl;
+
+
+    std::cout << "    q   Quit" << std::endl;
+
+    std::cout << std::endl << "Please press key: ";
+    std::cin >> answer;
+    std::cout << std::endl;
+    return answer;
 }
