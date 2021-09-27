2904d692b439405db90962ee990407022e6238ba
polymurph <eddyed.k@gmail.com>
2021-09-27 20:37:14 +0200

    cleanup project

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -4,163 +4,29 @@
 #include <unistd.h>
 #include <pthread.h>
 
-#include "reflowOven.hpp"
-#include "reflowOvenCtrl.hpp"
 
-pthread_t uI_th, fsm_th;
-
-
-char getCommand();
-
-void* userInterfaceTask(void* arg)
-{
-    char cmd;
-    do{
-        usleep(100000);
-        cmd = getCommand();
-
-        switch(cmd)
-        {
-            case 'a':
-                //reflowCtrl.process(ReflowOvenCtrl::Event::evAbort);
-                break;
-            case 'h':
-                //reflowCtrl.process(ReflowOvenCtrl::Event::evStartReflowProcess);
-                break;
-            case 's':
-                //reflowCtrl.process(ReflowOvenCtrl::Event::evTempReadyForSoack);
-                break;
-            case 'r':
-                //reflowCtrl.process(ReflowOvenCtrl::Event::evSoackTimePassed);
-                break;
-            case 'c':
-                //reflowCtrl.process(ReflowOvenCtrl::Event::evPeakTempReached);
-                break;
-            case 'e':
-                //reflowCtrl.process(ReflowOvenCtrl::Event::evSafeTempReached);
-                break;
-            default:
-                break;
-        }
-
-    }while(cmd != 'q');
-
-}
-
-void* finiteStateMachineTask(void* arg)
-{
-
-}
-
+void* delegator_thread(void* arg);
 
 int main(void)
 {
-    char cmd;
-    std::cout << "Reflow Controll Firmware" << std::endl;
+	pthread_t delegator_th_handle;
 
-    ReflowOvenCtrl reflowCtrl;
+	pthread_create(&delegator_th_handle,
+		       NULL,
+		       delegator_thread,
+		       NULL);
 
-    do{
-        cmd = getCommand();
-        switch(cmd)
-        {
-            case 'a':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evAbort);
-                break;
-            case 'h':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evStartReflowProcess);
-                break;
-            case 's':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evTempReadyForSoack);
-                break;
-            case 'r':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evSoackTimePassed);
-                break;
-            case 'c':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evPeakTempReached);
-                break;
-            case 'e':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evSafeTempReached);
-                break;
-            default:
-                break;
-        }
+	pthread_join(delegator_th_handle, NULL);
 
-    }while(cmd != 'q');
-
-
-
-    return 0;
+	return 0;
 }
 
-#ifdef Treads
-int main(void)
+void* delegator_thread(void* arg)
 {
-    char cmd;
-    std::cout << "Reflow Controll Firmware" << std::endl;
+	int i = 10;
 
-    ReflowOvenCtrl reflowCtrl;
-
-    std::cout << "User Interface Started" << std::endl;
-    pthread_create(&uI_th, NULL, userInterfaceTask, NULL);
-    //pthread_create(&fsm_th, NULL, finiteStateMachineTask, static_cast<void*>(&reflowCtrl));
-
-
-    pthread_join( uI_th, NULL);
-    //pthread_join( fsm_th, NULL);
-
-    return 0;
-
-    do{
-        cmd = getCommand();
-        switch(cmd)
-        {
-            case 'a':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evAbort);
-                break;
-            case 'h':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evStartReflowProcess);
-                break;
-            case 's':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evTempReadyForSoack);
-                break;
-            case 'r':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evSoackTimePassed);
-                break;
-            case 'c':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evPeakTempReached);
-                break;
-            case 'e':
-                reflowCtrl.process(ReflowOvenCtrl::Event::evSafeTempReached);
-                break;
-            default:
-                break;
-        }
-
-    }while(cmd != 'q');
-
-
-
-    return 0;
-}
-#endif // Threads
-
-char getCommand()
-{
-    char answer;
-    std::cout << std::endl << "-------------------------------------------" << std::endl;
-    std::cout << "    a   Abort" << std::endl;
-    std::cout << "    h   heatup" << std::endl;
-    std::cout << "    s   soack" << std::endl;
-    std::cout << "    r   ramp" << std::endl;
-    std::cout << "    c   cooldown" << std::endl;
-    std::cout << "    e   safe temperature reached" << std::endl;
-
-    std::cout << "    q   Quit" << std::endl;
-
-    std::cout << std::endl << "Please press key: ";
-    std::cin >> answer;
-    std::cout << std::endl;
-    system("clear");
-    return answer;
+	for(;i > 0; i--) {
+		std::cout << i << std::endl;
+	}
+	return NULL;
 }
